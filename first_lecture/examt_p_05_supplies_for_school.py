b445a84ca483831f94e5fbe691a3fb9b015e5356
mariami57 <mmitova3@gmail.com>
2024-05-09 16:38:24 +0300

    more solutions added

---
+++ first_lecture/examt_p_05_supplies_for_school.py
@@ -0,0 +1,13 @@
+n_pencils = int(input())
+n_markers = int(input())
+litres_of_detergent = int(input())
+discount = int(input())
+
+pencils_total_cost = n_pencils * 5.8
+markers_total_cost = n_markers * 7.2
+detergent_total_cost = litres_of_detergent * 1.2
+
+price_before_discount = pencils_total_cost + markers_total_cost + detergent_total_cost
+price_after_discount = price_before_discount - (price_before_discount * (discount / 100))
+
+print(price_after_discount)
---
+++ first_lecture/inches_to_centimeters.py
@@ -0,0 +1,5 @@
+inches = float(input())
+
+centimeteres = inches * 2.54
+
+print(centimeteres)
